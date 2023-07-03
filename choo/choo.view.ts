4f8d588c10a64d2d63db9b1433367ac2f60566b1
milis <milis@MacBook-Pro-mikhail.local>
2023-07-03 19:30:50 +0300

    rus

--- choo/choo.view.ts
+++ choo/choo.view.ts
@@ -2,7 +2,8 @@ namespace $.$$ {
 	export class $tw_choo extends $.$tw_choo {
 		result() {
 			const result_value  = this.damage() * this.speed()
-			return `${result_value} урона в секунду`
+			const result_value_with_penetration = result_value * this.penetration()
+			return `${result_value.toFixed(2)} урона в секунду по одной цели\n${result_value_with_penetration.toFixed(2)} урона в секунду по ${this.penetration()} целям`;
 		}
 	}
 }
