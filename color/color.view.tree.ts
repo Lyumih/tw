0f0eafa890906ac5e7c4aa39a5220d489dcc7a9e
Mikhail Ivanov <m.ivanov1225@gmail.com>
2023-07-05 20:38:12 +0300

    generate color

--- color/color.view.tree.ts
+++ color/color.view.tree.ts
@@ -1,7 +1,20 @@
 namespace $.$$ {
 	export class $tw_color extends $.$tw_color {
+		@ $mol_action
 		toggle( next?: any ) {
 			this.mode(!this.mode());
+		}
+		bg_from(): string {
+			return this.generate_color();
+		}
+		bg_target(): string {
+			return this.generate_color();
+		}
+		generate_color(): string {
+		    const randomColor = Math.floor(Math.random() * 16777215);
+			const hexColor = randomColor.toString(16);
+			const paddedHexColor = hexColor.padStart(6, '0');
+			return `#${paddedHexColor}`;
 		}
 	}
 
